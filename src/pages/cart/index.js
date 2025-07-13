4ecf68226a2c482eba925f5fb6dbdb9e91c5da0a
Adivam <adivamlopes@outlook.com>
2025-07-13 09:14:23 -0300

    Corrige erro de export default na página /cart

---
+++ src/pages/cart/index.js
@@ -0,0 +1,9 @@
+// pages/cart/index.js
+
+export default function CartPage() {
+    return (
+        <div style={{ padding: "2rem", fontSize: "1.5rem" }}>
+            Página de Carrinho em construção.
+        </div>
+    );
+}
